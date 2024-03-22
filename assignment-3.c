7c454b790d3be5ec6a9f29b2c847256114d228aa
impala1213 <84124472+impala1213@users.noreply.github.com>
2024-03-22 14:40:15 +0900

    Add files via upload

---
+++ assignment-3.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int id;
+    float weight;
+    int height;
+    float score;
+} Data;
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    Data *arr = (Data *)malloc(sizeof(Data) * n);
+
+    FILE *fp_txt = fopen("input.txt", "w");
+    if (fp_txt == NULL) {
+        printf("input.txt File Open Error\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %f %d %f", &arr[i].id, &arr[i].weight, &arr[i].height, &arr[i].score);
+        fprintf(fp_txt, "%d %f %d %f\n", arr[i].id, arr[i].weight, arr[i].height, arr[i].score);
+    }
+    fclose(fp_txt);
+
+    fp_txt = fopen("input.txt", "r");
+    FILE *fp_bin = fopen("test.bin", "wb");
+    if (fp_txt == NULL || fp_bin == NULL) {
+        printf("File Open Error\n");
+        exit(1);
+    }
+
+    Data temp;
+    while (fscanf(fp_txt, "%d %f %d %f", &temp.id, &temp.weight, &temp.height, &temp.score) != EOF) {
+        fwrite(&temp, sizeof(Data), 1, fp_bin);
+    }
+
+    fclose(fp_txt);
+    fclose(fp_bin);
+
+    fp_bin = fopen("test.bin", "rb");
+    if (fp_bin == NULL) {
+        printf("test.bin File Open Error\n");
+        exit(1);
+    }
+
+    fseek(fp_bin, k, SEEK_SET);
+    Data output;
+    fread(&output, sizeof(Data), 1, fp_bin);
+
+    printf("%0.1f %d %0.1f %d\n", output.score, output.id, output.weight, output.height);
+
+    fclose(fp_bin);
+    free(arr);
+
+    return 0;
+}
